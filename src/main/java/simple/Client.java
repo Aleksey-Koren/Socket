dbc5ce515d67a54c05fecbe78cdc9ec48f766609
Aliaksei Koran <akoran@scnsoft.com>
2022-02-03 18:09:12 +0300

    Client and Server

--- src/main/java/simple/Client.java
+++ src/main/java/simple/Client.java
@@ -16,31 +16,26 @@ public class Client {
             Socket socket = new Socket(InetAddress.getLocalHost(), 1010);
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
             DataInputStream in = new DataInputStream(socket.getInputStream());
-            byte[] buffer = new byte[256];
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
             while (true) {
                 String keyboardInput = reader.readLine();
                 out.write(keyboardInput.getBytes(StandardCharsets.UTF_8));
                 out.flush();
 
-                Thread.sleep(5000);
-
-                byte[] bytes = new byte[2000];
+                byte[] bytes = new byte[20];
                 int num;
-                String str = "";
+                StringBuilder builder = new StringBuilder();
                 while (true) {
                     num = in.read(bytes);
-                    str = str + new String(bytes, 0, num);
+
+                    builder.append(new String(bytes, 0, num));
                     if (in.available() < 1) {
                         break;
                     }
                 }
-                System.out.println(str);
-//                int length = in.read(buffer);
-//                System.out.println(new String(buffer, 0, length));
+                System.out.println(builder);
             }
-
-        } catch (IOException | InterruptedException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
--- src/main/java/simple/Server.java
+++ src/main/java/simple/Server.java
@@ -1,5 +1,8 @@
 package simple;
 
+import com.google.common.primitives.Bytes;
+import org.apache.commons.lang3.ArrayUtils;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -7,6 +10,10 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Server {
 
@@ -25,21 +32,31 @@ public class Server {
                 byte[] bytes = new byte[20];
                 int num = 0;
                 int count = 0;
+                List<Byte> bytesList = new ArrayList<>();
                 while (true) {
                     num = in.read(bytes);
-                    System.out.println(new String(bytes));
-                    out.write(bytes,0, num);
-                    count++;
+                    addToList(bytesList, bytes, num);
+                    System.out.println(new String(bytes, 0 , num));
+                    if(num > 0) {
+                        count++;
+                    }
                     if (in.available() < 1) {
                         break;
                     }
                 }
                 if (count > 0) {
+                    out.write(Bytes.toArray(bytesList));
                     out.flush();
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void addToList(List<Byte> bytesList, byte[] bytes, int num) {
+        for(int i = 0; i < num ; i++) {
+            bytesList.add(bytes[i]);
         }
     }
 }
